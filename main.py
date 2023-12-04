9f7b22d7461adb0fe74d00c76ca1bc06f46547aa
Eknoor Singh Eden <eknoorsingheden@eknoors-imac.lan>
2023-12-03 22:46:00 -0800

    wall collision, snake growth, snake collision

--- main.py
+++ main.py
@@ -30,10 +30,20 @@ while game_is_on:
 
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
 
     #detect collision with wall
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
         game_is_on = False
+        scoreboard.game_over()
+    
+    #Detect collision with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()    
--- snake.py
+++ snake.py
@@ -1,4 +1,4 @@
-from turtle import Turtle
+from turtle import Turtle, position
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 UP = 90
@@ -14,12 +14,18 @@ class Snake:
         self.head = self.segments[0]
 
     def create_snake(self):
-        for positon in STARTING_POSITIONS:
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def add_segment(self, position):
             new_segment = Turtle("square")
             new_segment.color("white")
             new_segment.penup()
-            new_segment.goto(positon)
+            new_segment.goto(position)
             self.segments.append(new_segment)
+        
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
